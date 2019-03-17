d1ff50396f0f2b6d9419c4554d6c75c641c6e1d2
“raghuvararora” <raghuvararora@gmail.com>
2019-03-18 04:09:45 +0530

    removed typos

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,6 +1,14 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 const display = calculator.querySelector('.calculator__display')
+
+/**
+ * Calcultes the value according to the operator
+ * @param {string} n1 :operand
+ * @param {string} operator : operator
+ * @param {string} n2 : second operand
+ * @returns {number} : result of the operation 
+ */
 const calculate = (n1, operator, n2) => {
   let result = ''
   if (operator == 'add'){
@@ -19,9 +27,14 @@ var secondValue = 0
 var previousKeyType = 'number'
 var operator = ''
 
+
+/**
+ * adds click eventlitener to every button
+ */
 keys.addEventListener('click', e=>{
-  
+  //if the button contains the class "number"
   if (e.target.classList.contains('number')){
+  	//checks if display==0 or the lastpressedkey is an operator
     if(display.textContent == '0' || previousKeyType == 'operator' ){
       clear.textContent = 'C'
       display.textContent = e.target.textContent
@@ -29,15 +42,17 @@ keys.addEventListener('click', e=>{
       display.textContent = display.textContent + e.target.textContent
     }
   }
-  
+  //if the button is a decimal button
   if (e.target.dataset.action == 'decimal'){
+  	//checks of the display already has a deciaml
     if(display.textContent.indexOf('.') == -1) {
       display.textContent = display.textContent + e.target.textContent
       clear.textContent = 'C'
     }
   }
-  
+  //if the button is a plus-
   if (e.target.dataset.action == 'plus-minus'){
+  	//checks if the display==0
     if(display.textContent != '0'){
       display.textContent = display.textContent * (-1)
     }
@@ -49,6 +64,7 @@ keys.addEventListener('click', e=>{
     }
   }
   
+  //resets the display and calulator
   if (e.target == clear) {
     firstValue = 0
     secondValue = 0
@@ -59,10 +75,13 @@ keys.addEventListener('click', e=>{
       .forEach(k => k.classList.remove('is-selected'))
   }
   
+  
   if(e.target.dataset.action == 'add' ||
       e.target.dataset.action == 'subtract' ||
       e.target.dataset.action == 'multiply' ||
-      e.target.dataset.action == 'divide'){
+      e.target.dataset.action == 'divide')
+  {
+
     operator = e.target.dataset.action
     Array.from(e.target.parentNode.children)
       .forEach(k => k.classList.remove('is-selected'))
